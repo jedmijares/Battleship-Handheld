73d5ce0702875cd941f6247dc31c0d5dfac16c88
Jed Mijares <36108020+jedmijares@users.noreply.github.com>
2019-04-08 17:03:57 -0400

    removed unnecessary stuff

--- main.c
+++ main.c
@@ -8,19 +8,6 @@
 #include "tm4c123gh6pm.h"
 #include "squares.h"
 #include "printMatrix.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-//int main(void){
-//	unsigned short xpos = 0;
-//	unsigned short ypos = 0;
-//	short buttons = 0;
-//  PLL_Init();                           // set system clock to 80 MHz
-//  Nokia5110_Init();
-//  SysTick_Init();
-//  PortF_Init();
-//  Nokia5110_Clear();
 
 Sea board[8][8];
 short xCursor = 0;
@@ -58,12 +45,11 @@ int main(void)
 {		
 	int i, j;
 	int shotsFired = 0;
-	int goodShots = 0;
-	char* shotsString;//[sizeof(int)*8+1];
+	//int goodShots = 0;
+	//char* shotsString;//[sizeof(int)*8+1];
 	Sea board[8][8];
 	short xCursor = 0;
 	short yCursor = 0;
-	//short buttons = 0x11;
 	short reading; // reading of buttons
 	short buttonState;
 	short oldReading = 0x11; // previous reading of buttons
@@ -108,11 +94,10 @@ int main(void)
 				}
 				if(buttonState == 0x01) 
 				{
-					if(board[xCursor][yCursor].isHit == 0 & board[xCursor][yCursor].isShip == 1) goodShots++;
-					board[xCursor][yCursor].isHit = 1;
+					//if(board[xCursor][yCursor].isHit == 0 & board[xCursor][yCursor].isShip == 1) goodShots++;
+					fire();
 					print(board);
 					select(board, xCursor, yCursor);
-					fire();
 					Nokia5110_DisplayBuffer();
 					shotsFired++;
 				}
@@ -120,7 +105,7 @@ int main(void)
 			
 		}
 		
-		oldReading = reading;
+		oldReading = reading; // for button debouncing
 		//sprintf(shotsString, "%d", shotsFired);
 		Nokia5110_SetCursor(8,0);
 		//Nokia5110_OutUDec(shotsFired);
@@ -129,40 +114,3 @@ int main(void)
 		Nokia5110_DisplayBuffer();
 	}
 }
-
-//int main(void)
-//{
-//	short reading; // reading of buttons
-//	short buttonState;
-//	short oldReading = 0x11; // previous reading of buttons
-//	unsigned long lastDebounceTime = 0;
-//	unsigned long debounceDelay = 50; // time to wait before more button input
-//	unsigned long bInt = 1000; // time to toggle blue
-//	unsigned long bPrevious = 0;
-//	
-//	PLL_Init();
-//	PortF_Init();
-//	SysTick_Init(80000); // interrupt/toggle every 80,000 cycles (1 ms at 80 MHz)
-//	
-//	while(1)
-//	{
-//		if((millis() - bPrevious) >= bInt)
-//		{
-//			bPrevious = millis();
-//			blueToggle();
-//		}
-//		
-//		reading = pushbuttons(); // read value of buttons
-//		if( reading != oldReading ) lastDebounceTime = millis(); // if reading does not match last value, we're still bouncing
-//		if( (millis() - lastDebounceTime) > debounceDelay) // if the time since the last bounce is greater than the delay, step in
-//		{
-//			if (reading != buttonState) // if the reading does not match what the computer thinks the button was last, step in
-//			{
-//				buttonState = reading;
-//				if(buttonState == 0x10)
-//					greenToggle();
-//			}
-//		}
-//		oldReading = reading;
-//	}
-//}
