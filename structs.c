115752e52d32100b88d69e0406deea872abd3565
Jed Mijares <36108020+jedmijares@users.noreply.github.com>
2019-04-20 18:40:40 -0400

    added x-y randomization

--- structs.c
+++ structs.c
@@ -6,7 +6,28 @@ void randomPlaceShip(struct Ship ship, struct Square board[8][8])
 	ship.hits = 0;
 	ship.sunk = false;
 	ship.xpos = (Random32()%(8-ship.length+1));
-	ship.ypos = 0;
+	ship.ypos = (Random32()%(8-ship.length+1));
+	
+	for(int i = ship.xpos; (i-ship.xpos) < ship.length; i++) // checking if this ship will overlap with another ship
+	{
+		if(!ship.isVertical)
+		{
+			if(board[i][ship.ypos].isShip) // if a ship already occupies one of this ship's squares
+			{
+				randomPlaceShip(ship, board); // place the ship somewhere else
+				return; // and stop
+			}
+		}
+		else
+		{
+			if(board[ship.xpos][i].isShip)
+			{
+				randomPlaceShip(ship, board);
+				return;
+			}
+		}
+	}
+	
 	for(int i = ship.xpos; (i-ship.xpos) < ship.length; i++)
 	{
 		if(!ship.isVertical)
@@ -14,6 +35,8 @@ void randomPlaceShip(struct Ship ship, struct Square board[8][8])
 			board[i][ship.ypos].isShip = true;
 		}
 		else
+		{
 			board[ship.xpos][i].isShip = true;
+		}
 	}
 }
